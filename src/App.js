0eb930f23f97b96ca22569b2abfecc2a6ffd50c5
Juancapp <juan.cappuccio@radiumrocket.com>
2023-04-13 10:03:30 -0300

    cambio 3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1>¡Esta es la rama develop!</h1>
       <h2>¡Este es el cambio 1!</h2>
       <h2>¡Este es el cambio 2!</h2>
+      <h2>¡Este es el cambio 3!</h2>
     </div>
   );
 }
