821b1abeab29bce59943e3f0506e49198aee8f8e
Juancapp <juan.cappuccio@radiumrocket.com>
2023-04-13 10:05:14 -0300

    ¡Cambio para rebasear feature nuevamente

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
       <h1>¡Esta es la rama develop!</h1>
       <h2>¡Este es el cambio 1!</h2>
       <h2>¡Este es el cambio 2!</h2>
-      <h2>¡Este es el cambio 3!</h2>
+      <h2>
+        ¡Este es el cambio para rebasear feature nuevamente y chequear el Log!
+      </h2>
     </div>
   );
 }
